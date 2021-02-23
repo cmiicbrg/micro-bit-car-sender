470dea5fa70cb4f7dba0a03fff3654871324d07c
Christoph Stadlbauer <christoph.stadlbauer@brgenns.ac.at>
2021-02-23 18:00:03 +0100

    Richtung anzeigen

--- main.ts
+++ main.ts
@@ -1,10 +1,31 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
     radio.sendString("stop")
 })
 input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
     radio.sendString("vor")
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
     radio.sendString("zurück")
 })
 input.onButtonPressed(Button.B, function () {
