81dcace9a5be427ba473f6a79b87cd32abf50deb
Christoph Stadlbauer <christoph.stadlbauer@brgenns.ac.at>
2021-02-24 09:28:15 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,19 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         `)
     radio.sendString("stop")
 })
+function zeigeRichtung () {
+    if (r == 2) {
+        basic.showArrow(ArrowNames.East)
+    } else if (r == 1) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (r == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else if (r == -1) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else if (r == -2) {
+        basic.showArrow(ArrowNames.West)
+    }
+}
 input.onSound(DetectedSound.Loud, function () {
     basic.showLeds(`
         . . . . .
@@ -48,6 +61,7 @@ input.onButtonPressed(Button.B, function () {
         `)
     radio.sendString("schneller")
 })
+let r = 0
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -58,5 +72,11 @@ basic.showLeds(`
 radio.setGroup(1)
 input.setSoundThreshold(SoundThreshold.Loud, 160)
 basic.forever(function () {
-	
+    if (10 < input.rotation(Rotation.Pitch) && input.rotation(Rotation.Pitch) < 80) {
+        r = Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), -45, 45), -45, 45, -2, 2))
+    } else {
+        r = 0
+    }
+    zeigeRichtung()
+    radio.sendNumber(r)
 })
